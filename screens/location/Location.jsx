520e8198209249a43738672b6b5d7a36eabf2c0e
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-08-02 23:32:19 +0530

    Created Map Screen

--- screens/location/Location.jsx
+++ screens/location/Location.jsx
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
 const Location = () => {
+
+  const coordinates = {
+    // latitude: 35.6855,
+    // longitude: 139.68884,
+    // latitudeDelta: 0.01,
+    // longitudeDelta: 0.01,
+    latitude: 7.8731,
+    longitude: 80.7718,
+    latitudeDelta: 2.5,
+    longitudeDelta: 2.5,
+    title: "Colombo"
+
+  }
+
   return (
-    <View>
-      <Text>Location</Text>
-    </View>
+    <MapView provider={PROVIDER_GOOGLE} initialRegion={coordinates} style={styles.mapStyle}>
+      <Marker coordinate={coordinates} title={coordinates.title} description={"Capital of Sri Lanka"}/>
+    </MapView>
   )
 }
 
 export default Location
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mapStyle: {
+    ...StyleSheet.absoluteFillObject
+  }
+})
